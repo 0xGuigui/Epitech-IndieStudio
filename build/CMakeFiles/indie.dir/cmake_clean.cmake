da3a0c4a1f539a0221539cca0506657208afb45b
Julien <julien.hennion@epitech.eu>
2022-05-25 20:22:45 +0200

    [+] Initial commit, cmake and raylib window

---
+++ build/CMakeFiles/indie.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/indie.dir/src/main.cpp.o"
+  "indie"
+  "indie.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/indie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/indie.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for indie.
+# This may be replaced when dependencies are built.
