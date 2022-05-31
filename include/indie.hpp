30b3838d914cc55351119a94f7d01d890df8f254
Nysioko <nysioko@gmail.com>
2022-05-31 17:48:54 +0200

    [+] - Added Indie class, updated SplashScreen but not working anymore

---
+++ include/indie.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2022
+** B-YEP-400-LIL-4-1-indiestudio-paul.gervais
+** File description:
+** splashscreen
+*/
+
+#pragma once
+
+#include "window.hpp"
+
+class Indie {
+    public:
+        Indie();
+        ~Indie();
+        void displaySplashScreen();
+    protected:
+    private:
+        bmb::Window _window;
+        Music _music;
+};
---
+++ src/indie.cpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2022
+** B-YEP-400-LIL-4-1-indiestudio-paul.gervais
+** File description:
+** splashscreen
+*/
+
+#include "indieStudio.hpp"
+
+Indie::Indie() {}
+
+Indie::~Indie() {}
+
+void Indie::displaySplashScreen() {
+    Texture2D splashScreen = LoadTexture("assets/SplashScreen/images/splashscreen.png");
+    InitAudioDevice();
+    this->_music = LoadMusicStream("assets/SplashScreen/sounds/splashscreen.ogg");
+    this->_music.looping = false;
+    splashScreen.width = 1920;
+    splashScreen.height = 1080;
+    int x = (GetScreenWidth() - splashScreen.width) / 2;
+    int y = (GetScreenHeight() - splashScreen.height) / 2;
+    DrawTexture(splashScreen, x, y, WHITE);
+    PlayMusicStream(this->_music);
+    UpdateMusicStream(this->_music);
+    return;
+}
