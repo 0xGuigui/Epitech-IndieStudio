f693b8c01677a055d469ebe8e0cca16a3ea1a20b
Nysioko <nysioko@gmail.com>
2022-06-01 18:16:45 +0200

    [~] - Updated mainMenu, music and buttons work

--- src/indie.cpp
+++ src/indie.cpp
@@ -11,46 +11,6 @@ Indie::Indie() {}
 
 Indie::~Indie() {}
 
-static void AudioProcessEffectLPF(void *buffer, unsigned int frames)
-{
-    static float low[2] = { 0.0f, 0.0f };
-    static const float cutoff = 70.0f / 44100.0f; // 70 Hz lowpass filter
-    const float k = cutoff / (cutoff + 0.1591549431f); // RC filter formula
-
-    for (unsigned int i = 0; i < frames*2; i += 2)
-    {
-        float l = ((float *)buffer)[i], r = ((float *)buffer)[i + 1];
-        low[0] += k * (l - low[0]);
-        low[1] += k * (r - low[1]);
-        ((float *)buffer)[i] = low[0];
-        ((float *)buffer)[i + 1] = low[1];
-    }
-}
-
-static float *delayBuffer = NULL;
-static unsigned int delayBufferSize = 0;
-static unsigned int delayReadIndex = 2;
-static unsigned int delayWriteIndex = 0;
-
-// Audio effect: delay
-static void AudioProcessEffectDelay(void *buffer, unsigned int frames)
-{
-    for (unsigned int i = 0; i < frames*2; i += 2)
-    {
-        float leftDelay = delayBuffer[delayReadIndex++];    // ERROR: Reading buffer -> WHY??? Maybe thread related???
-        float rightDelay = delayBuffer[delayReadIndex++];
-
-        if (delayReadIndex == delayBufferSize) delayReadIndex = 0;
-
-        ((float *)buffer)[i] = 0.5f*((float *)buffer)[i] + 0.5f*leftDelay;
-        ((float *)buffer)[i + 1] = 0.5f*((float *)buffer)[i + 1] + 0.5f*rightDelay;
-
-        delayBuffer[delayWriteIndex++] = ((float *)buffer)[i];
-        delayBuffer[delayWriteIndex++] = ((float *)buffer)[i + 1];
-        if (delayWriteIndex == delayBufferSize) delayWriteIndex = 0;
-    }
-}
-
 void Indie::displaySplashScreen() {
 	static Texture2D SplashScreenImage = LoadTexture("assets/SplashScreen/images/splashscreen.png");
 	static Sound SplashScreenSound = LoadSound("assets/SplashScreen/audios/splashscreen.ogg");
@@ -74,8 +34,16 @@ void Indie::displaySplashScreen() {
 
 void Indie::displayMainMenu() {
     static Texture2D MainMenuImage = LoadTexture("assets/MainMenu/images/mainmenu.png");
+	static Texture2D TitleImage = LoadTexture("assets/MainMenu/images/IndieCraft.png");
+	static Texture2D singleplayer = LoadTexture("assets/MainMenu/images/buttons/singleplayer.png");
+	static Texture2D multiplayer = LoadTexture("assets/MainMenu/images/buttons/multiplayer.png");
+	static Texture2D options = LoadTexture("assets/MainMenu/images/buttons/options.png");
+	static Texture2D quitgame = LoadTexture("assets/MainMenu/images/buttons/quitgame.png");
+	static Texture2D accessibility = LoadTexture("assets/MainMenu/images/buttons/accessibility.png");
+
     static Music MainMenuMusic = LoadMusicStream("assets/MainMenu/audios/Moog-City.mp3");
     static bool isPlaying = false;
+    MainMenuMusic.looping = true;
 
 
     if (!isPlaying) {
@@ -84,8 +52,18 @@ void Indie::displayMainMenu() {
     }
     MainMenuImage.width = 1920;
     MainMenuImage.height = 1080;
-    int x = (GetScreenWidth() - MainMenuImage.width) / 2;
-    int y = (GetScreenHeight() - MainMenuImage.height) / 2;
-    DrawTexture(MainMenuImage, x, y, WHITE);
+    int mainMenuBackground_x = (GetScreenWidth() - MainMenuImage.width) / 2;
+    int mainMenuBackground_y = (GetScreenHeight() - MainMenuImage.height) / 2;
+	int middle_x = (GetScreenWidth() - TitleImage.width) / 2;
+	int middle_y = (GetScreenHeight() - TitleImage.height) / 2;
+	// int title_y = (GetScreenHeight() - TitleImage.height) / 2;
+    DrawTexture(MainMenuImage, mainMenuBackground_x, mainMenuBackground_y, WHITE);
+	DrawTexture(TitleImage, middle_x, 75, WHITE);
+	DrawTexture(singleplayer, middle_x, middle_y + 50, WHITE);
+	DrawTexture(multiplayer, middle_x, middle_y + 150, WHITE);
+	DrawTexture(options, middle_x, middle_y + 250, WHITE);
+	DrawTexture(quitgame, middle_x + 408, middle_y + 250, WHITE);
+	DrawTexture(accessibility, middle_x + 825, middle_y + 250, WHITE);
+    UpdateMusicStream(MainMenuMusic);
 }
 
