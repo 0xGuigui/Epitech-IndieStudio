96a67f0d226fcf34f1b9aa7189cebd0374b3459c
mflr0 <mflr0@onionmail.org>
2022-06-01 16:54:44 +0200

    [+] - Added some textures, buttons and more

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
-make clean
-make
-./bomberman
+##
+## EPITECH PROJECT, 2022
+## B-YEP-400-LIL-4-1-indiestudio-paul.gervais
+## File description:
+## build
+##
+
+cmake . && make clean && make && ./bomberman
