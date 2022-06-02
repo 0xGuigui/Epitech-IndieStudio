65c6add2e603a63d72707545116b51f7282e9a67
Julien <julien.hennion@epitech.eu>
2022-06-02 16:59:27 +0200

    [~] Fixed screen.hpp

--- include/screen.hpp
+++ include/screen.hpp
@@ -21,25 +21,25 @@ namespace bmb {
             int GetHeight() {
                 return GetScreenHeight();
             }
-            Ray GetMouseRay(Vector2 mousePosition, Camera camera) {
+            Ray GetMouseRay(IndieVector2 mousePosition, IndieCamera camera) {
                 return ::GetMouseRay(mousePosition, camera);
             }
-            Matrix GetCameraMatrix(Camera camera) {
+            Matrix GetCameraMatrix(IndieCamera camera) {
                 return ::GetCameraMatrix(camera);
             }
-            Matrix GetCameraMatrix2D(Camera camera) {
+            Matrix GetCameraMatrix2D(IndieCamera camera) {
                 return ::GetCameraMatrix2D(camera);
             }
-            IndieVector2 GetWorldToScreen(Vector3 position, Camera camera) {
+            IndieVector2 GetWorldToScreen(IndieVector3 position, IndieCamera camera) {
                 return ::GetMousePosition(position, camera);
             }
-            IndieVector2 GetWorldToScreenEx(Vector3 position, Camera camera) {
+            IndieVector2 GetWorldToScreenEx(IndieVector3 position, IndieCamera camera) {
                 return ::GetWorldToScreenEx(position, camera);
             }
-            IndieVector2 GetWorldToScreen2D(Vector2 position, Camera camera) {
+            IndieVector2 GetWorldToScreen2D(IndieVector2 position, IndieCamera camera) {
                 return ::GetWorldToScreen2D(position, camera);
             }
-            IndieVector2 GetScreenToWorld2D(Vector2 position, Camera camera) {
+            IndieVector2 GetScreenToWorld2D(IndieVector2 position, IndieCamera camera) {
                 return ::GetScreenToWorld2D(position, camera);
             }
 		private:
