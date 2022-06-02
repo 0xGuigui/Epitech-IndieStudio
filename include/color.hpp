165661e92c755e486a307b994d0d08f2312340c4
Florian Garnier <florian.garnier@epitech.eu>
2022-06-02 16:27:54 +0200

    [+] color.hpp

---
+++ include/color.hpp
@@ -0,0 +1,40 @@
+/*
+** EPITECH PROJECT, 2022
+** color.hpp
+** File description:
+** include
+*/
+
+#include "includes.hpp"
+
+namespace bmb {
+	class IndieColor {
+		public:
+			IndieColor(unsigned char r, unsigned char g, unsigned char b, unsigned char a) {
+				this->color = {
+					.r = r,
+					.g = g,
+					.b = b,
+					.a = a
+				};
+			}
+			// From HSV
+			IndieColor(float hue, float saturation, float value) {
+				this->color = ColorFromHSV(hue, saturation, value);
+			}
+			IndieColor(unsigned int hexValue) {
+				this->color = GetColor(hexValue);
+			}
+			void Fade(float alpha) {
+				this->color = ::Fade(this->color, alpha);
+			}
+			int ToInt() {
+				return ColorToInt(this->color);
+			}
+			void Alpha(float alpha) {
+				this->color = ColorAlpha(this->color, alpha);
+			}
+		private:
+			Color color;
+	};
+};
