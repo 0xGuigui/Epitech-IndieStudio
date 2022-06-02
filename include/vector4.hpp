727f5c63f44133403e3e405e12fa05b4fb3a4f15
Julien <julien.hennion@epitech.eu>
2022-06-02 16:55:49 +0200

    [+] Added vector3 and vector 4 files and encap

---
+++ include/vector4.hpp
@@ -0,0 +1,40 @@
+/*
+** EPITECH PROJECT, 2022
+** vector
+** File description:
+** vector
+*/
+
+#pragma once
+
+#include <raylib.h>
+
+namespace bmb {
+	class IndieVector4 {
+		public:
+            IndieVector4(float x, float y, float z, float w) {
+                _vector.y = y;
+                _vector.x = x;
+                _vector.z = z;
+                _vector.w = w;
+            }
+			~IndieVector4() {};
+            float getX() {
+                return _vector.x;
+            }
+            float getY() {
+                return _vector.y;
+            }
+            float getZ() {
+                return _vector.z;
+            }
+            float getW() {
+                return _vector.w;
+            }
+            operator Vector4 () {
+                return _vector;
+            }
+		private:
+            Vector4 _vector;
+	};
+};
