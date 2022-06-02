24b0c653d1109fb3b0bcb47c076546eef8bac6c5
Julien <julien.hennion@epitech.eu>
2022-06-02 17:47:18 +0200

    [~] fixed came2D.hpp

--- include/camera2D.hpp
+++ include/camera2D.hpp
@@ -13,8 +13,10 @@ namespace bmb {
 	class IndieCamera2D {
 		public:
             IndieCamera2D(IndieVector2 offset, IndieVector2 target, float rotation, float zoom) {
-                _vector.y = y;
-                _vector.x = x;
+                _camera2D.zoom = zoom;
+                _camera2D.rotation = rotation;
+                _camera2D.target = target;
+                _camera2D.offset = offset;
             }
 			~IndieCamera2D() {};
             float getRotation() {
