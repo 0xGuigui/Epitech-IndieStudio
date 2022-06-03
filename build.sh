ab8a1a10a82351311117cbedb70c1b9a0a1812e9
Gagonlaire <paul.gervais@epitech.eu>
2022-06-03 13:46:14 +0200

    facto: update scripts

--- build.sh
+++
@@ -1,8 +0,0 @@
-##
-## EPITECH PROJECT, 2022
-## B-YEP-400-LIL-4-1-indiestudio-paul.gervais
-## File description:
-## build
-##
-
-cmake . && make clean && make && ./bomberman
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,10 @@
+#!/bin/bash
 ##
 ## EPITECH PROJECT, 2022
 ## B-YEP-400-LIL-4-1-indiestudio-paul.gervais
 ## File description:
 ## build
 ##
-
-#!/bin/bash
 
 if [ $1 == "--run" ] || [ $1 == "-r" ]; then
   cmake . && make && ./bomberman
