416e2951d27fcb3acb6d13627057ee6e2e678649
Julien <julien.hennion@epitech.eu>
2022-06-03 16:05:01 +0200

    [~] fixed camera include in screen.hpp

--- include/encapsulation/ray.hpp
+++ include/encapsulation/ray.hpp
@@ -12,11 +12,14 @@
 namespace bmb {
     class IndieRay {
     public:
+        IndieRay(const Ray &ray) : _ray(ray) {};
+        IndieRay(bool hit, float distance) {
+            _ray.hit = false;
+            _ray.distance = 0;
+        };
         operator Ray() const {
             return _ray;
         }
-
-        IndieRay(const Ray &ray) : _ray(ray) {};
     private:
         Ray _ray;
     };
