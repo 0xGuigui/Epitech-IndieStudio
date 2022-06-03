7f2f54f3cd712c9a7d89795da35e2668f4e57a53
Florian Garnier <florian.garnier@epitech.eu>
2022-06-03 16:57:39 +0200

    [~] indieStudio.hpp

--- include/indieStudio.hpp
+++ include/indieStudio.hpp
@@ -7,8 +7,21 @@
 
 #pragma once
 
+#include "encapsulation/audioDevice.hpp"
+#include "encapsulation/camera.hpp"
+#include "encapsulation/color.hpp"
+#include "encapsulation/draw.hpp"
 #include "encapsulation/image.hpp"
-#include "encapsulation/music.hpp"
+#include "encapsulation/matrix.hpp"
+#include "encapsulation/model.hpp"
+#include "encapsulation/npatchInfo.hpp"
+#include "encapsulation/ray.hpp"
+#include "encapsulation/rectangle.hpp"
+#include "encapsulation/screen.hpp"
+#include "encapsulation/sound.hpp"
 #include "encapsulation/texture.hpp"
+#include "encapsulation/vector.hpp"
 #include "encapsulation/window.hpp"
 #include "indie.hpp"
+#include "includes.hpp"
+#include "Map.hpp"
