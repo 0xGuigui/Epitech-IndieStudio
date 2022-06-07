e0bcf5e6fa57d4e716ccd89f137b882a881d725c
Gagonlaire <paul.gervais@epitech.eu>
2022-06-07 15:43:45 +0200

    feat: add resource loader

---
+++ include/components/resourceLoader.hpp
@@ -0,0 +1,138 @@
+/*
+** EPITECH PROJECT, 2022
+** resourceLoader.hpp
+** File description:
+** resourceLoader.hpp
+*/
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include "error.hpp"
+#include "../external/robin_hood.hpp"
+
+namespace bmb {
+    class IResourceConnector {
+    public:
+        virtual ~IResourceConnector() = default;
+
+        [[nodiscard]] virtual const std::vector<std::string> &getPaths() const = 0;
+
+        virtual void addResources(const std::filesystem::directory_entry &entry) = 0;
+    };
+
+    template<class T>
+    class ResourceConnector : public IResourceConnector {
+    private:
+        const std::vector<std::string> _paths;
+        const std::vector<std::string> _fileExtensions;
+        robin_hood::unordered_flat_map<std::string, T> _resources;
+
+    public:
+        ResourceConnector() = delete;
+
+        explicit ResourceConnector(const std::string &subPath, std::vector<std::string> fileExtensions = {}) :
+                _paths(std::vector<std::string>{subPath}),
+                _fileExtensions(std::move(fileExtensions)) {};
+
+        explicit ResourceConnector(std::vector<std::string> subPaths, std::vector<std::string> fileExtensions = {}) :
+                _paths(std::move(subPaths)),
+                _fileExtensions(std::move(fileExtensions)) {};
+
+        ~ResourceConnector() override = default;
+
+        /*
+         * Get list of lookup paths
+         */
+        [[nodiscard]] const std::vector<std::string> &getPaths() const override {
+            return _paths;
+        }
+
+        /*
+         * Get list of acceptable file
+         */
+        [[nodiscard]] const std::vector<std::string> &getFileExtensions() const {
+            return _fileExtensions;
+        }
+
+        /*
+         * Manually add a resource by giving a path to it
+         */
+        void addResource(const std::string &name, const std::string &path) {
+            if (_resources.contains(name)) {
+                throw ResourceConnectorException("A resource with the \"" + name + "\" already exists");
+            }
+            _resources.insert({name, std::move(T(path))});
+        }
+
+        /*
+         * Manually add a resource by giving an object
+         */
+        void addResource(const std::string &name, T resource) {
+            if (_resources.contains(name)) {
+                throw ResourceConnectorException("A resource with the \"" + name + "\" already exists");
+            }
+            _resources.insert({name, std::move(resource)});
+        }
+
+        /*
+         * Add resource using the cpp modern filesystem
+         */
+        void addResources(const std::filesystem::directory_entry &entry) override {
+            if (!entry.is_directory()) {
+                throw ResourceConnectorException("Error: \"" + entry.path().string() + "\" is not a directory");
+            }
+            for (auto &file : std::filesystem::recursive_directory_iterator(entry)) {
+                if (!file.is_regular_file()) {
+                    continue;
+                }
+                if (_fileExtensions.empty() || std::find(_fileExtensions.begin(), _fileExtensions.end(),
+                                                         file.path().extension().string()) != _fileExtensions.end()) {
+                    std::cout << file.path().extension().string() << std::endl;
+                    this->addResource(file.path().stem().string(), file.path().string());
+                }
+            }
+        }
+
+        /*
+         * Used to access an element of the resource map
+         */
+        T &operator[](const std::string &name) {
+            auto value = _resources.find(name);
+
+            if (value == _resources.end()) {
+                throw ResourceConnectorException("Resource \"" + name + "\" does not exist");
+            }
+            return value->second;
+        }
+    };
+
+    class ResourceLoader {
+    public:
+        // List of all connectors
+
+    private:
+        // Add your connectors here for building process
+        std::vector<std::reference_wrapper<IResourceConnector>> _connectors = {};
+
+    public:
+        explicit ResourceLoader(const std::string &rootFolderName) {
+            std::string rootPath = std::filesystem::current_path().string() + "/" + rootFolderName;
+
+            for (auto &file: std::filesystem::directory_iterator(rootPath)) {
+                if (!file.is_directory()) {
+                    continue;
+                }
+                for (auto i : _connectors) {
+                    const std::vector<std::string> &paths = i.get().getPaths();
+
+                    if (std::find(paths.begin(), paths.end(), file.path().stem().string()) != paths.end()) {
+                        i.get().addResources(file);
+                    }
+                }
+            }
+        }
+    };
+}
