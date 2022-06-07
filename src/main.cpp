fa8bb81e8a06f6ebaff2510f0b1808735bafa939
Florian Garnier <florian.garnier@epitech.eu>
2022-06-07 20:59:45 +0200

    [+] Randomized map generation

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,8 @@ int main(void) {
 
     indie.window.setFPS(60);
 
-	indie.map = bmb::MapController("assets/InGame/map.png", "assets/InGame/block.png", "assets/InGame/map_obstacles.png", "assets/InGame/brick.png", { -16.0f, 0.0f, -8.0f}, { { 0.0f, 17.0f, 5.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 0 });
+    srand(time(NULL));
+	indie.map = bmb::MapController("assets/InGame/map.png", "assets/InGame/block.png", "assets/InGame/map_obstacles.png", "assets/InGame/brick.png", { -16.0f, 0.0f, -8.0f}, { { -20.0f, 20.0f, -1.0f }, { -9.0f, 0.0f, -1.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 0 });
     for (; !indie.window.isClosed(); indie.timePlayed += GetFrameTime()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
