bc2edb01ac6e8bc7acc0e5dbca453159adbaa519
Florian Garnier <florian.garnier@epitech.eu>
2022-06-07 20:59:45 +0200

    [+] Randomized map generation

--- src/Game.cpp
+++ src/Game.cpp
@@ -9,13 +9,36 @@
 
 using namespace bmb;
 
-// std::vector<Vector3[2]> generateMap() {
-//     // TODO
-// }
+void generateLineHorizontal(std::vector<IndieVector3> &map, float posY, float posX, float posXEnd) {
+    for (float x = posX; x <= posXEnd; x++) {
+        if (rand() % 4 > 0)
+            map.push_back({ x, 0.5f, posY });
+    }
+}
+
+void generateLineVertical(std::vector<IndieVector3> &map, float posX, float posY, float posYEnd) {
+    for (float y = posY; y <= posYEnd; y++) {
+        if (rand() % 4 > 0)
+            map.push_back({ posX, 0.5f, y });
+    }
+}
+
+std::vector<IndieVector3> generateBoxes() {
+    std::vector<IndieVector3> map;
+
+    generateLineVertical(map, -3.0f, -5.0f, 3.0f);
+    generateLineVertical(map, -15.0f, -5.0f, 3.0f);
+    generateLineHorizontal(map, 5.0f, -13.0f, -5.0f);
+    generateLineHorizontal(map, -7.0f, -13.0f, -5.0f);
+    for (float x = -13.0f; x < -3.0f; x += 2.0f)
+        generateLineVertical(map, x, -7.0f, 5.0f);
+    for (float y = -5.0f; y < 5.0f; y += 2.0f)
+        generateLineHorizontal(map, y, -15.0f, -3.0f);
+    return map;
+}
 
 void Indie::bomberMan() {
-    const int screenWidth = this->screen.GetWidth();
-    const int screenHeight = this->screen.GetHeight();
+    static std::vector<IndieVector3> map = generateBoxes();
 
     if (!this->map.isCameraUpdated()) {
         this->map.setCameraMode(CAMERA_FREE);
@@ -23,7 +46,9 @@ void Indie::bomberMan() {
     this->map.updateCamera();
     this->map.begin3D();
     this->map.Draw();
-    // // DrawCube(BoxPos, BoxSize.getVector().x, BoxSize.getVector().y, BoxSize.getVector().z, GRAY);
-    // // DrawCubeWires(BoxPos, BoxSize.getVector().x, BoxSize.getVector().y, BoxSize.getVector().z, DARKGRAY);
+    for (IndieVector3 &BoxPos : map) {
+        DrawCube(BoxPos, 1.0f, 1.0f, 1.0f, GRAY);
+        DrawCubeWires(BoxPos, 1.0f, 1.0f, 1.0f, DARKGRAY);
+    }
     this->map.end3D();
 }
