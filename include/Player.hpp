003eb6f82f7af2df451e8bcb7baf06759a9c2cb1
Florian Garnier <florian.garnier@epitech.eu>
2022-06-08 16:57:49 +0200

    [~] Player class

--- include/Player.hpp
+++ include/Player.hpp
@@ -7,8 +7,21 @@
 
 #include "indieStudio.hpp"
 
+//ModelGetBoundingBox
+
 namespace bmb {
 	class Player {
-		
+		private:
+			IndieModel playerModel;
+		public:
+			Player() = default;
+			Player(std::string path) {
+				IndieTexture2D texture("assets/InGame/scene.png");
+				playerModel.LoadModel("assets/InGame/scene.gltf");
+				playerModel.getModel().materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
+			}
+			void Draw() {
+				playerModel.Draw({-15.0f, 0.5f, -5.0f}, 1.0f, WHITE);
+			}
 	};
 }
