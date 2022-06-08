6f722de5cfe5a840b839ca5c3c725ad8fd030916
Florian Garnier <florian.garnier@epitech.eu>
2022-06-08 13:29:10 +0200

    [~] Fix camera mode and generation

--- src/Game.cpp
+++ src/Game.cpp
@@ -9,38 +9,9 @@
 
 using namespace bmb;
 
-void generateLineHorizontal(std::vector<IndieVector3> &map, float posY, float posX, float posXEnd) {
-    for (float x = posX; x <= posXEnd; x++) {
-        if (rand() % 4 > 0)
-            map.push_back({ x, 0.5f, posY });
-    }
-}
-
-void generateLineVertical(std::vector<IndieVector3> &map, float posX, float posY, float posYEnd) {
-    for (float y = posY; y <= posYEnd; y++) {
-        if (rand() % 4 > 0)
-            map.push_back({ posX, 0.5f, y });
-    }
-}
-
-std::vector<IndieVector3> generateBoxes() {
-    std::vector<IndieVector3> map;
-
-    generateLineVertical(map, -3.0f, -5.0f, 3.0f);
-    generateLineVertical(map, -15.0f, -5.0f, 3.0f);
-    generateLineHorizontal(map, 5.0f, -13.0f, -5.0f);
-    generateLineHorizontal(map, -7.0f, -13.0f, -5.0f);
-    for (float x = -13.0f; x < -3.0f; x += 2.0f)
-        generateLineVertical(map, x, -7.0f, 5.0f);
-    for (float y = -5.0f; y < 5.0f; y += 2.0f)
-        generateLineHorizontal(map, y, -15.0f, -3.0f);
-    return map;
-}
-
 void Indie::bomberMan() {
-    if (!this->map.isCameraUpdated()) {
-        this->map.setCameraMode(CAMERA_FREE);
-    }
+    if (!this->map.isCameraUpdated())
+        this->map.setCameraMode(CAMERA_PERSPECTIVE);
     this->map.updateCamera();
     this->map.begin3D();
     this->map.Draw();
