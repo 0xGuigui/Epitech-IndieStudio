2243cf4173c5612e2a4f5f4234778af757b90eb6
Florian Garnier <florian.garnier@epitech.eu>
2022-06-08 16:57:49 +0200

    [~] Player class

--- src/Game.cpp
+++ src/Game.cpp
@@ -5,15 +5,18 @@
 ** Menus
 */
 
-#include "indieStudio.hpp"
+#include "Player.hpp"
 
 using namespace bmb;
 
 void Indie::bomberMan() {
+    static Player player("");
+
     if (!this->map.isCameraUpdated())
         this->map.setCameraMode(CAMERA_PERSPECTIVE);
     this->map.updateCamera();
     this->map.begin3D();
     this->map.Draw();
+    player.Draw();
     this->map.end3D();
 }
