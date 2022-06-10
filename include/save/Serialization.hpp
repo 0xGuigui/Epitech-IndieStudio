fdcd8bba29c813a77192c670bcfde92e72de8918
PAUL Antoine <antoine1.paul@epitech.eu>
2022-06-10 15:10:01 +0200

    [+] save init file, serialization/deserialization

---
+++ include/save/Serialization.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2022
+** Game.hpp
+** File description:
+** serialization.hpp
+*/
+
+#pragma once
+
+#include "indieStudio.hpp"
+
+namespace bmb {
+    class Serialization {
+        Serialization();
+        ~Serialization();
+    };
+}
