80edafbec9215b92266b9de0d00a26e256637442
Gagonlaire <paul.gervais@epitech.eu>
2022-06-12 22:28:40 +0200

    feat: add fully working environment handler

--- include/components/environmentHandler.hpp
+++ include/components/environmentHandler.hpp
@@ -7,41 +7,85 @@
 
 #pragma once
 
+#include "components/error.hpp"
 #include "encapsulation/model.hpp"
+#include "encapsulation/color.hpp"
+#include "encapsulation/vector.hpp"
 #include "vector"
 #include <string>
+#include <sstream>
 #include <fstream>
 #include <iostream>
 #include <algorithm>
 
-class EnvironmentHandler {
-private:
-    std::vector<Model> _models;
+#define CHUNK_FLOATING_POINT 10
 
-public:
-    EnvironmentHandler() = default;
+namespace bmb {
+    class RenderChunk {
+    private:
+        short _x;
+        short _y;
+        short _z;
+        short _count;
+        char _scale;
 
-    ~EnvironmentHandler() = default;
+    public:
+        RenderChunk(float x, float y, float z, int count, float scale) {
+            this->_x = static_cast<short>(x * CHUNK_FLOATING_POINT);
+            this->_y = static_cast<short>(y * CHUNK_FLOATING_POINT);
+            this->_z = static_cast<short>(z * CHUNK_FLOATING_POINT);
+            this->_count = static_cast<short>(count);
+            this->_scale = static_cast<char>(scale * CHUNK_FLOATING_POINT);
+        }
 
-    std::string& ltrim(std::string &s);
+        IndieVector3 getPosition() const {
+            return {static_cast<float>(this->_x) / CHUNK_FLOATING_POINT,
+                    static_cast<float>(this->_y) / CHUNK_FLOATING_POINT,
+                    static_cast<float>(this->_z) / CHUNK_FLOATING_POINT};
+        }
 
-    /*
-     * Parse and alloc ressources for data
-     */
-    void parseHeader(std::string &header);
+        int getCount() const {
+            return this->_count;
+        }
 
-    /*
-     * Add new bloc
-     */
-    void parseLine(std::string &line);
+        float getScale() const {
+            return static_cast<float>(this->_scale) / CHUNK_FLOATING_POINT;
+        }
+    };
 
-    /*
-     * Open and parse config file for environment
-     */
-    void init(const std::string &configPath = "data/environment.indie");
+    typedef std::tuple<IndieModel &, std::vector<RenderChunk>> ModelRenderChunks;
 
-    /*
-     * Draw all blocks in the world
-     */
-    void draw();
-};
+    class EnvironmentHandler {
+    private:
+        std::vector<ModelRenderChunks> _renderData;
+        std::unordered_map<std::string, bmb::IndieModel> fakeMap = {
+                {"grass_block", bmb::IndieModel("assets/models/grass_block.glb")},
+                {"tnt", bmb::IndieModel("assets/models/tnt.glb")},
+                {"stone", bmb::IndieModel("assets/models/stone.glb")},
+                {"dirt", bmb::IndieModel("assets/models/dirt.glb")},
+                {"cobblestone", bmb::IndieModel("assets/models/cobblestone.glb")},
+        };
+
+    public:
+        EnvironmentHandler() = default;
+
+        ~EnvironmentHandler() = default;
+
+        std::string& ltrim(std::string &s);
+
+        /*
+         * Open and parse config file for environment
+         */
+        void init(const std::string &configPath = "data/environment.indie");
+
+        /*
+         * Add a new block to the render process
+         */
+        void addBlock(IndieModel &model, bmb::RenderChunk &chunk);
+
+        /*
+         * Draw all blocks in the world
+         */
+        void draw();
+    };
+}
--- src/components/environmentHandler.cpp
+++ src/components/environmentHandler.cpp
@@ -7,7 +7,7 @@
 
 #include "components/environmentHandler.hpp"
 
-std::string &EnvironmentHandler::ltrim(std::string &s) {
+std::string &bmb::EnvironmentHandler::ltrim(std::string &s) {
     auto it = std::find_if(s.begin(), s.end(),
                            [](char c) {
                                return !std::isspace<char>(c, std::locale::classic());
@@ -16,21 +16,61 @@ std::string &EnvironmentHandler::ltrim(std::string &s) {
     return s;
 }
 
-void EnvironmentHandler::init(const std::string &configPath) {
+void bmb::EnvironmentHandler::init(const std::string &configPath) {
     std::fstream file(configPath);
+    std::string line;
+    std::string modelName;
+    float x, y, z;
+    int count;
+    float scale;
 
     if (!file.is_open())
         throw std::runtime_error("Can't open file " + configPath);
+    while (std::getline(file, line)) {
+        ltrim(line);
+        if (line.empty() || line[0] == '#')
+            continue;
+        std::istringstream iss(line);
+        if (!(iss >> x >> y >> z >> count >> scale >> modelName)) {
+            throw bmb::EnvironmentHandlerException("Invalid line: '" + line += "'");
+        }
+        // Get model
+        // @TODO use the resource loader
+        auto it = fakeMap.find(modelName);
+        if (it != fakeMap.end()) {
+            bmb::RenderChunk newChunk(x, y, z, count, scale);
+
+            addBlock(it->second, newChunk);
+        } else {
+            throw bmb::EnvironmentHandlerException("Unknown model: " + modelName += " in line: '" + line += "'");
+        }
+    }
 }
 
-void EnvironmentHandler::parseHeader(std::string &header) {
+void bmb::EnvironmentHandler::addBlock(bmb::IndieModel &model, bmb::RenderChunk &chunk) {
+    auto it = std::find_if(_renderData.begin(), _renderData.end(),
+                           [&model](const ModelRenderChunks &data) {
+                               return std::get<0>(data) == model;
+                           });
 
+    if (it == _renderData.end()) {
+        _renderData.push_back(ModelRenderChunks(model, std::vector<RenderChunk>{chunk}));
+    } else {
+        std::get<1>(*it).push_back(chunk);
+    }
 }
 
-void EnvironmentHandler::parseLine(std::string &line) {
+void bmb::EnvironmentHandler::draw() {
+    for (auto &data: _renderData) {
+        for (auto &chunk: std::get<1>(data)) {
+            IndieVector3 position = chunk.getPosition();
+            int count = chunk.getCount();
+            float scale = chunk.getScale();
 
-}
-
-void EnvironmentHandler::draw() {
-
+            for (int i = 0; i < count; i++) {
+                std::get<0>(data).Draw(position, scale, WHITE);
+                position.incrementZ(1);
+            }
+        }
+    }
 }
