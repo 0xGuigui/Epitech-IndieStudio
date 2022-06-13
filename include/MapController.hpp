b9c1e4886278cb2bcb0267123d37eb8dfa7582bd
Julien <julien.hennion@epitech.eu>
2022-06-13 18:47:09 +0200

    [~] gen powerups in the map

--- include/MapController.hpp
+++ include/MapController.hpp
@@ -9,6 +9,7 @@
 
 #include "encapsulation/model.hpp"
 #include "encapsulation/camera.hpp"
+#include "PowerUp.hpp"
 #include <vector>
 
 namespace bmb {
@@ -19,6 +20,7 @@ namespace bmb {
 			IndieModel _destructible;
 			IndieCamera3D _camera;
 			IndieVector3 _position;
+			std::vector<IndiePowerUp> _bonuses = {};
 			std::vector<IndieVector3> _destructiblePositions = {};
 			bool _updateCamera = false;
 			void generateLineHorizontal(float posY, float posX, float posXEnd, int percentage) {
@@ -66,6 +68,24 @@ namespace bmb {
 				_position = mapPosition;
 				_camera = camera;
 				this->generateBoxes(75);
+				for (auto& destructiblePosition : _destructiblePositions) {
+					// if random qui génère des PowerUp et qui push dans bonusPositions
+					int probability = (rand() % 100) - 100;
+					switch (probability) {
+						case probability > 0 && probability < 50:
+							_bonuses.push_back(IndiePowerUp(destructiblePosition, FIREUP));
+							break;
+						case probability > 50 && probability < 75:
+							_bonuses.push_back(destructiblePosition, BOMBUP);
+							break;
+						case probability > 75 && probability < 100:
+							_bonuses.push_back(destructiblePosition, SPEEDUP);
+							break;
+						default:
+							break;
+					}
+
+				}
 			};
 			void setPosition(const IndieVector3& position) {
 				_position = position;
