b58477a7171cf397262566fe96063d08cbb2c3bb
mflr0 <mflr0@onionmail.org>
2022-06-15 14:52:05 +0200

    [+] - Added endScreen

--- src/Menus/EndMenu.cpp
+++ src/Menus/EndMenu.cpp
@@ -13,4 +13,22 @@ using namespace bmb;
 
 void Indie::displayEndMenu() {
     static IndieTexture2D endMenuBackground = loader.textures["winpage"];
+    static float middle_x = (this->screen.GetWidth() - endMenuBackground.getWidth()) / 2;
+    static float middle_y = (this->screen.GetHeight() - endMenuBackground.getHeight()) / 2;
+    static IndieTexture2D backToMenu = loader.textures["quit_to_title"];
+    static IndieSound buttonSound = loader.sounds["button"];
+    static IndieSound winSound = loader.sounds["fireworks"];
+
+    if (!winSound.isPlaying())
+        winSound.Play();
+
+    static IndieButton backToMenuButton({middle_x + 275, middle_y + 920, static_cast<float>(backToMenu.getWidth()), static_cast<float>(backToMenu.getHeight())},
+                                        {middle_x + 275, middle_y + 920}, backToMenu, loader.textures["quit_to_title_highlight"], [&]() -> void
+                                        {
+        buttonSound.Play();
+        winSound.Stop();
+        this->state = mainMenu; });
+
+    endMenuBackground.Draw(middle_x, middle_y, WHITE);
+    backToMenuButton.update();
 }
