a71b475ce7f5b74f12c9b4068f63463d9f0d3f0e
Florian Garnier <florian.garnier@epitech.eu>
2022-06-15 14:10:02 +0200

    [~] Removed casts

--- src/Menus/PlayMenu.cpp
+++ src/Menus/PlayMenu.cpp
@@ -20,14 +20,14 @@ void Indie::displayPlayMenu()
     static IndieSound buttonSound = loader.sounds["button"];
     static IndieSound closeSound = loader.sounds["close"];
     static IndieMusic MainMenuMusic = loader.musics["Moog-City-2"];
-    static IndieButton playButtonObject((IndieRectangle){middle_x + 200, middle_y + 850, static_cast<float>(playButton.getWidth()), static_cast<float>(playButton.getHeight())},
-                                        (IndieVector2){middle_x + 200, middle_y + 850}, playButton, loader.textures["play_highlight"], [&]() -> void
+    static IndieButton playButtonObject({middle_x + 200, middle_y + 850, static_cast<float>(playButton.getWidth()), static_cast<float>(playButton.getHeight())},
+                                        {middle_x + 200, middle_y + 850}, playButton, loader.textures["play_highlight"], [&]() -> void
                                         {
         buttonSound.Play();
         this->state = inGame;
         MainMenuMusic.Stop(); });
-    static IndieButton cancelButtonObject((IndieRectangle){middle_x + 1100, middle_y + 850, static_cast<float>(cancelButton.getWidth()), static_cast<float>(cancelButton.getHeight())},
-                                          (IndieVector2){middle_x + 1100, middle_y + 850}, cancelButton, loader.textures["cancel_highlight"], [&]() -> void
+    static IndieButton cancelButtonObject({middle_x + 1100, middle_y + 850, static_cast<float>(cancelButton.getWidth()), static_cast<float>(cancelButton.getHeight())},
+                                          {middle_x + 1100, middle_y + 850}, cancelButton, loader.textures["cancel_highlight"], [&]() -> void
                                           {
         buttonSound.Play();
         this->state = mainMenu; });
